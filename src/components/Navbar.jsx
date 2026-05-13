ba97bbc7ebeb16148483214f3e4e5b71017c5f2a
Zakaria-info <info.zakariahossain01@gnail.com>
2026-05-13 13:19:53 +0600

    Install all dependency and added banner and footer section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between p-5'>
+            <ul className='flex gap-3'>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/destinations">Destinations</Link></li>
+                <li><Link href="/my-bookings">My Bookings</Link></li>
+            </ul>
+            <div>
+                <Image src={"/assets/Wanderlast.png"} alt="Logo" width={150} height={150} />
+            </div>
+            <ul className='flex gap-3'>
+                <li><Link href="/profile">Profile</Link></li>
+                <li><Link href="/login">Login</Link></li>
+                <li><Link href="/signup">Signup</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
