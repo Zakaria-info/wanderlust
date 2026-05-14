9c60f309c3bcb4a40c61f6ecf7149def1de7bc57
Zakaria-info <info.zakariahossain01@gnail.com>
2026-05-14 14:41:22 +0600

    Added edit destination section and dynamic destination details id

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -1,5 +1,10 @@
+
+import { EditModal } from '@/components/EditModal';
+import { Button } from '@heroui/react';
 import Image from 'next/image';
 import React from 'react';
+import { FaRegCalendar } from 'react-icons/fa';
+import { LuMapPin } from 'react-icons/lu';
 
 const DestinationDetailsPage = async ({params}) => {
     const { id } =await params;
@@ -8,13 +13,27 @@ const DestinationDetailsPage = async ({params}) => {
     console.log(destination)
 
 
-    const { imageUrl, destinationName, price, duration, country } = destination;
+    const { imageUrl, destinationName, price, duration, country, description } = destination;
 
     console.log(id)
     return (
         <div className='max-w-7xl mx-auto mt-10'>
-            <Image src={imageUrl} alt={destinationName} width={800} height={500} />
+            <EditModal destination={destination} />
+            <Image className='w-full h-100 object-cover' src={imageUrl} alt={destinationName} width={800} height={500} />
 
+            <div className="flex gap-2 items-center">
+                      <LuMapPin /> <span>{country}</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                      <h2 className="text-xl font-bold">{destinationName}</h2>
+                        <p className="text-lg font-semibold text-green-500">${price}</p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <FaRegCalendar /> <span>{duration}</span>
+                    </div>
+                    <h1 className="text-2xl font-bold mt-6">Overview</h1>
+                    <p className="text-gray-600 mt-2">{description}</p>
+            
         </div>
     );
 };
